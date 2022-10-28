51338e740d39ea0fa062bfad53c8504e9655e319
Zihao Zhang <zzh8829@gmail.com>
2022-10-28 16:19:37 -0700

    Update index.js

--- node-express-demo-1/routes/index.js
+++ node-express-demo-1/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express", name: process.env.NAME });
+  res.render("index", { title: "Express 1", name: process.env.NAME });
 });
 
 module.exports = router;
